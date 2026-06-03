49316318da09aecfaf1262f0cf8fa744ce937809
ukadepoojasampat19 <poojau_t21080@students.isquareit.edu.in>
2026-06-03 21:09:19 +0530

    Sprint 1 completed - MongoDB setup"

---
+++ video-chat-app-master/backend/src/config/db.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const connectDB = async (): Promise<void> => {
+  try {
+    const conn = await mongoose.connect(
+      process.env.MONGODB_URI as string
+    );
+
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`
+    );
+  } catch (error) {
+    console.error("MongoDB Connection Failed:", error);
+
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- video-chat-app-master/backend/src/server.ts
+++ video-chat-app-master/backend/src/server.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db";
 
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+// Connect Database
+connectDB();
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({
