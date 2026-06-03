3f9bbf9c0e5ad2c045d5ee2d2aa724c7c1ffb975
ukadepoojasampat19 <poojau_t21080@students.isquareit.edu.in>
2026-06-03 20:17:31 +0530

    server started and with response 200 , healthy status found

---
+++ video-chat-app-master/backend/src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is running",
+  });
+});
+//http://localhost:5000/api/health
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
